83997ab8c78f8d16daad16d0ea798f80d5f3f400
Arek Stopczynski <arekquest@gmail.com>
2013-05-03 16:00:52 +0000

    updated EXAMPLE_SECURE_service_config.py

--- sensible_data_service/utils/EXAMPLE_SECURE_service_config.py
+++ sensible_data_service/utils/EXAMPLE_SECURE_service_config.py
@@ -1,6 +1,17 @@
 DATABASE= {
-		{
 			"username":"here_be_username",
 			"password":"here_be_password"
+}
+
+AUTH_DATABASE= {
+			"username":"hereh_be_username",
+			"password":"here_be_password"
+}
+
+CONNECTORS= {
+		"connector_funf": {
+			"db_pass": "here_be_password"
 		}
-	}
+
+
+}
