c5e8e87909e9bdd47dc01ff806914d522b42091e
arks <arks@lestrade.imm.dtu.dk>
2013-05-04 00:37:14 +0200

    added anonymizer object

--- sensible_data_service/connectors/connector/connector.py
+++ sensible_data_service/connectors/connector/connector.py
@@ -1,19 +1,14 @@
 import os, sys
-try:
-	lib_path = os.path.abspath('../../utils')
-	sys.path.append(lib_path)
-	from database import Database
-	import service_config
-	import SECURE_service_config
-except ImportError:
-	from utils.database import Database
-	from utils import service_config
-	from utils import SECURE_service_config
+from utils.database import Database
+from utils import service_config
+from utils import SECURE_service_config
+from anonymizer import anonymizer
 
 class Connector(object):
 	db = None
+	anonymizer = None
 
 	def __init__(self):
 		self.db = Database()
-
+		self.anonymizer = anonymizer.Anonymizer()
 
