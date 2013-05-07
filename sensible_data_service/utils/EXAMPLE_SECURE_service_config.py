d46c8f9c9cb5e91e4cb1a0d708801fd80a3fa234
Arek Stopczynski <arekquest@gmail.com>
2013-05-07 02:53:41 +0000

    updated example secure config

--- sensible_data_service/utils/EXAMPLE_SECURE_service_config.py
+++ sensible_data_service/utils/EXAMPLE_SECURE_service_config.py
@@ -1,17 +1,62 @@
 DATABASE= {
-			"username":"here_be_username",
-			"password":"here_be_password"
+			"username":"",
+			"password":""
 }
 
 AUTH_DATABASE= {
-			"username":"hereh_be_username",
-			"password":"here_be_password"
+			"username":"",
+			"password":""
 }
 
 CONNECTORS= {
 		"connector_funf": {
-			"db_pass": "here_be_password"
+			"db_pass": ""
 		}
 
+}
 
+PROBE_KEYS= {
+        "edu_mit_media_funf_probe_builtin_CellProbe": "",
+        "edu_mit_media_funf_probe_builtin_WifiProbe": "",
+        "edu_mit_media_funf_probe_builtin_BluetoothProbe": "",
+        "edu_mit_media_funf_probe_builtin_LocationProbe": "",
+        "edu_mit_media_funf_probe_builtin_CallLogProbe": "",
+        "edu_mit_media_funf_probe_builtin_SMSProbe": "",
+        "edu_mit_media_funf_probe_builtin_HardwareInfoProbe": "",
+        "edu_mit_media_funf_probe_builtin_ContactProbe": "",
+
+}
+
+
+#TODO
+PIIS= {
+	"edu_mit_media_funf_probe_builtin_CellProbe": {
+		"cid": "",
+		"lac": "",
+	},
+	"edu_mit_media_funf_probe_builtin_WifiProbe": {
+		"SCAN_RESULTS": {
+			"SSID": "",
+			"BSSID": "",
+		}
+	},
+	"edu_mit_media_funf_probe_builtin_BluetoothProbe": {
+		"DEVICES": {
+			"android_bluetooth_device_extra_DEVICE":{
+				"mAddress": ""
+			}
+		}
+	},
+}
+
+VALUE_KEYS= {
+	"user": "",
+	"device_id": "",
+	"bluetooth_mac": PROBE_KEYS["edu_mit_media_funf_probe_builtin_BluetoothProbe"],
+	"wifi_mac": PROBE_KEYS["edu_mit_media_funf_probe_builtin_WifiProbe"],
+	"android_id": "",
+}
+
+IV= {
+	"iv": ""
 }
