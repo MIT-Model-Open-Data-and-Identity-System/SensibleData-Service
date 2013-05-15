6e244a920426899686edd4e2a0a5f8465a36baf3
Arek Stopczynski <arekquest@gmail.com>
2013-05-15 01:31:12 +0000

    stub for identity_manager

---
+++ sensible_data_service/identity_manager/identity_manager.py
@@ -0,0 +1,10 @@
+from utils import service_config
+from utils import SECURE_service_config
+
+class IdentityManager(object):
+	def __init__(self):
+		pass
+
+	#TODO: exchange ticket for the user
+	def requestUserForTicket(self, user_ticket):
+		return 'too_user'
