1fd913d6821581c47d09cd0e481684d3bb468c8b
Arek Stopczynski <arekquest@gmail.com>
2013-05-15 01:31:36 +0000

    stub for platform_manager

---
+++ sensible_data_service/platform_manager/platform_manager.py
@@ -0,0 +1,10 @@
+from utils import service_config
+from utils import SECURE_service_config
+
+class PlatformManager(object):
+	def __init__(self):
+		pass
+	
+	def validateRequest(self, request):
+		#TODO: validate if the reuqest comes from the platform
+		return True
