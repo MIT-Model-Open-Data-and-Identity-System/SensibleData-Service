d3f19b518d93a4ae4dd059f333fd3c73affa0c58
Arek Stopczynski <arekquest@gmail.com>
2013-06-03 18:33:45 +0000

    new endpoints

--- sensible_data_service/sensible_data_service/urls.py
+++ sensible_data_service/sensible_data_service/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^connectors/', include('connectors.urls')),
 	url(r'^authorization_manager/', include('authorization_manager.urls')),
-	url(r'^application_manager/', include('application_manager.urls')),
+	#url(r'^application_manager/', include('application_manager.urls')), #used via django admin atm
+	url(r'^platform_api/', include('platform_manager.urls')),
+	url(r'^openid/', include('django_openid_auth.urls')),
+	url(r'^admin/', include(admin.site.urls)),
 	 
 	url(r'^test/', include('testing.urls')),
 )
