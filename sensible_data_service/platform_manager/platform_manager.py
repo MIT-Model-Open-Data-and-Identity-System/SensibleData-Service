35f5643e1d2e53bc5b695c087e8a9e1919616bb9
Arek Stopczynski <arekquest@gmail.com>
2013-06-05 13:56:08 +0000

    added string responses to json errors

--- sensible_data_service/platform_manager/platform_manager.py
+++ sensible_data_service/platform_manager/platform_manager.py
@@ -1,14 +1,15 @@
 from utils import service_config
 from utils import SECURE_service_config
 from .models import *
+import json
 
 def authenticate(request):
 	token = request.REQUEST.get('access_token')
         host = request.META['REMOTE_ADDR']
-	if not host in service_config.PLATFORM['ip_addr']: return {'error': 'ip address not authorized'}
+	if not host in service_config.PLATFORM['ip_addr']: return {'error': 'ip address not authorized', 'response': json.dumps({'error': 'ip address not authorized'})}
 
 	try: user = AccessToken.objects.get(token=token).user
-        except AccessToken.DoesNotExist: return {'error': 'no user found'}
+        except AccessToken.DoesNotExist: return {'error': 'no user found', 'response': json.dumps({'error': 'no user found'})}
 
 	#TODO: logging
 	return {'ok':'authenticated', 'user': user}
