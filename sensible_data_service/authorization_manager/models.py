154c5135b296e3a03723f27146c7f9ce90e40165
Arek Stopczynski <arekquest@gmail.com>
2013-06-05 13:48:51 +0000

    added models to authorization manager

---
+++ sensible_data_service/authorization_manager/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+from connectors.models import *
+from application_manager.models import *
+
+class Authorization(models.Model):
+	user = models.ForeignKey(User)
+	scope = models.ForeignKey(Scope)
+	application = models.ForeignKey(Application)
+	payload = models.TextField(null=True, blank=True)
+	active = models.BooleanField()
+	created_at = models.PositiveIntegerField(null=True)
+	revoked_at = models.PositiveIntegerField(null=True)
