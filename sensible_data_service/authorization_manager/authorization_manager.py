b9b4e51b60ba4be9ef5bd6b3dad0ac67ea8be06f
Arek Stopczynski <arekquest@gmail.com>
2013-06-15 21:23:53 +0000

    added build uri dispatcher

--- sensible_data_service/authorization_manager/authorization_manager.py
+++ sensible_data_service/authorization_manager/authorization_manager.py
@@ -1,5 +1,11 @@
 from .models import *
+from connectors.connector_funf import auth
 
 def getAuthorization(user, scope, application):
 	authorizations = Authorization.objects.filter(active=True, user=user, scope=scope, application=application)
 	return authorizations
+
+
+def buildUri(connector, application):
+	if connector.name == 'connector_funf':
+		return auth.buildUri(connector, application)
