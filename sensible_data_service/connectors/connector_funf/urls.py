d76402d9e2ba7823b7718c06adcc141af38045cc
Arek Stopczynski <arekquest@gmail.com>
2013-06-15 21:28:39 +0000

    added new endpoints

--- sensible_data_service/connectors/connector_funf/urls.py
+++ sensible_data_service/connectors/connector_funf/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
 	 url(r'^upload/', 'connectors.connector_funf.connector_funf.upload'),
 	 url(r'^config/', 'connectors.connector_funf.connector_funf.config'),
+	
+	 url(r'^auth/grant/', 'connectors.connector_funf.auth.grant'),
+	 url(r'^auth/revoke/', 'connectors.connector_funf.auth.revoke'),
+	 url(r'^auth/sync/', 'connectors.connector_funf.auth.sync'),
+	 url(r'^auth/confirm/', 'connectors.connector_funf.auth.confirm'),
 )
