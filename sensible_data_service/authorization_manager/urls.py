737c8c8d115fe2638259ce2c2902f05caa861dba
Arek Stopczynski <arekquest@gmail.com>
2013-06-15 21:22:00 +0000

    move auth to connector

--- sensible_data_service/authorization_manager/urls.py
+++ sensible_data_service/authorization_manager/urls.py
@@ -1,11 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('authorization_manager',
-	url(r'^grant/connector_funf/?', 'connector_funf.grant'),
-	url(r'^revoke/connector_funf/?', 'connector_funf.revoke'),
-	url(r'^sync/connector_funf/?', 'connector_funf.sync'),
+urlpatterns = patterns('',
+	url(r'^connector_funf/?', include('connectors.connector_funf.urls')),
 )
