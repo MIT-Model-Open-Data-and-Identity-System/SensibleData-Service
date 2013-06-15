bb08e54b930e1af238f2559c1268416854603ec7
Arek Stopczynski <arekquest@gmail.com>
2013-06-15 21:12:41 +0000

    added gcm registration to admin

--- sensible_data_service/authorization_manager/admin.py
+++ sensible_data_service/authorization_manager/admin.py
@@ -5,3 +5,8 @@ class AuthorizationAdmin(admin.ModelAdmin):
 	list_display = ('user', 'application', 'scope', 'active', 'created_at', 'revoked_at')
 
 admin.site.register(Authorization, AuthorizationAdmin)
+
+class GcmRegistrationAdmin(admin.ModelAdmin):
+	list_display = ('user', 'device_id', 'gcm_id')
+
+admin.site.register(GcmRegistration, GcmRegistrationAdmin)
