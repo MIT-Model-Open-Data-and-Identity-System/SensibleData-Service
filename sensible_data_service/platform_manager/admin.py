36f5805bacf28665d7a59868327aa928dcfffe1a
Arek Stopczynski <arekquest@gmail.com>
2013-06-15 21:31:35 +0000

    changed models not to conflict with application authorization manager

--- sensible_data_service/platform_manager/admin.py
+++ sensible_data_service/platform_manager/admin.py
@@ -1,22 +1,22 @@
 from .models import *
 from django.contrib import admin
 
-class ScopeAdmin(admin.ModelAdmin):
+class PlatformScopeAdmin(admin.ModelAdmin):
         list_display = ('key', 'description')
         class Meta:
                 verbose_name = 'scope'
 
-class CodeAdmin(admin.ModelAdmin):
+class PlatformCodeAdmin(admin.ModelAdmin):
         list_display = ('user', 'code', 'exchanged', 'time_generated', 'time_exchanged')
         class Meta:
                 verbose_name = 'authorization code'
 
-class AccessTokenAdmin(admin.ModelAdmin):
+class PlatformAccessTokenAdmin(admin.ModelAdmin):
         list_display = ('user', 'token', 'token_type', 'refresh_token', 'expire')
         class Meta:
                 verbose_name = 'acess token'
 
 
-admin.site.register(Scope, ScopeAdmin)
-admin.site.register(Code, CodeAdmin)
-admin.site.register(AccessToken, AccessTokenAdmin)
+admin.site.register(PlatformScope, PlatformScopeAdmin)
+admin.site.register(PlatformCode, PlatformCodeAdmin)
+admin.site.register(PlatformAccessToken, PlatformAccessTokenAdmin)
