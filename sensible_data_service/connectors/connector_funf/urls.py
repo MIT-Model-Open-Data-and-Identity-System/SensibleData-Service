3572fb468cccb74629be4b7e44613ae93bc030a7
Arek Stopczynski <arekquest@gmail.com>
2013-06-16 22:40:15 +0000

    added granted endpoint

--- sensible_data_service/connectors/connector_funf/urls.py
+++ sensible_data_service/connectors/connector_funf/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
 	 url(r'^config/', 'connectors.connector_funf.connector_funf.config'),
 	
 	 url(r'^auth/grant/', 'connectors.connector_funf.auth.grant'),
+	 url(r'^auth/granted/', 'connectors.connector_funf.auth.granted'),
 	 url(r'^auth/revoke/', 'connectors.connector_funf.auth.revoke'),
 	 url(r'^auth/sync/', 'connectors.connector_funf.auth.sync'),
 	 url(r'^auth/confirm/', 'connectors.connector_funf.auth.confirm'),
