ff380415369e56d62316e54c120488b3f13dce99
Arek Stopczynski <arekquest@gmail.com>
2013-06-16 22:38:18 +0000

    added oauth2 urls

--- sensible_data_service/authorization_manager/urls.py
+++ sensible_data_service/authorization_manager/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^connector_funf/?', include('connectors.connector_funf.urls')),
+	url(r'^oauth2/?', include('authorization_manager.oauth2_urls')),
 )
