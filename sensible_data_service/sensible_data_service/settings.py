d965c4a1b9e5dd0b29634ca0e228e7cb492a2fdc
Arek Stopczynski <arekquest@gmail.com>
2013-06-18 01:23:05 +0000

    switched to mysql backend

--- sensible_data_service/sensible_data_service/settings.py
+++ sensible_data_service/sensible_data_service/settings.py
@@ -16,13 +16,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(BASE_DIR, 'SECURE_db.sqlite3'),                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+	'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR,'SECURE_mysql.cnf'),
+        },
     }
 }
 
