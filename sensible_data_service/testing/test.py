01bdbfd358d2ea9e9ed83acfdb089672d290e31c
Arek Stopczynski <arekquest@gmail.com>
2013-07-08 15:59:23 +0000

    just for testing

--- sensible_data_service/testing/test.py
+++ sensible_data_service/testing/test.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from authorization_manager.authorization_manager import *
 import bson.json_util as json
 from connector_pipes.connector_pipe_funf import connector_pipe_funf
 
+@login_required
 def testing(request):
-	response = 'dupa'
-	return HttpResponse(json.dumps(response))
+	response = str(request.user.username)
+	return HttpResponse(response)
 
