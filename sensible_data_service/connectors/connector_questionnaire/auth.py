7f8c925eabe8e4047e542cbc1ca0f9d212aeecc8
Arek Stopczynski <arekquest@gmail.com>
2013-07-08 15:58:19 +0000

    added response_type

--- sensible_data_service/connectors/connector_questionnaire/auth.py
+++ sensible_data_service/connectors/connector_questionnaire/auth.py
@@ -12,10 +12,11 @@ def grant(request):
         except AttributeError: return HttpResponse(json.dumps({"error":"no scope provided"}))	
 	client_id = request.REQUEST.get('client_id', '')
 	state = request.REQUEST.get('state', '')
+	response_type = request.REQUEST.get('response_type', '')
 
 	redirect_uri = '/authorization_manager/oauth2/authorize/?'
         redirect_uri += 'client_id='+client_id
-        redirect_uri += '&response_type=token'
+        redirect_uri += '&response_type='+response_type
         redirect_uri += '&scope='+','.join(scope)
         redirect_uri += '&redirect_uri='+Client.objects.get(key=client_id).redirect_uri+'&state='+state
 	
