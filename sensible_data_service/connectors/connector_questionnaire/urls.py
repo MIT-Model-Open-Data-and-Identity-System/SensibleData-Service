6c8b3cf93c8814df3dae9df7bafec666518c0d5b
Arek Stopczynski <arekquest@gmail.com>
2013-07-10 11:25:22 +0000

    new questionnaire connector urls

--- sensible_data_service/connectors/connector_questionnaire/urls.py
+++ sensible_data_service/connectors/connector_questionnaire/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('connectors.connector_questionnaire',
-         #url(r'^upload/', 'connectors.connector_funf.connector_funf.upload'),
+         url(r'^upload/', 'connector_questionnaire.upload'),
 
          url(r'^auth/grant/', 'auth.grant'),
-#         url(r'^auth/granted/', 'auth.granted'),
+         url(r'^auth/token/', 'auth.token'),
 #         url(r'^auth/revoke/', 'auth.revoke'),
 #         url(r'^auth/sync/', 'auth.sync'),
-#         url(r'^auth/confirm/', 'auth.confirm'),
 )
