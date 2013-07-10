2d149217857c9fd25f1297fece14dc769f5868f9
Arek Stopczynski <arekquest@gmail.com>
2013-07-10 08:48:24 +0000

    removed pipes

--- sensible_data_service/testing/test.py
+++ sensible_data_service/testing/test.py
@@ -5,7 +5,6 @@ from django.contrib.auth.decorators import login_required
 
 from authorization_manager.authorization_manager import *
 import bson.json_util as json
-from connector_pipes.connector_pipe_funf import connector_pipe_funf
 
 @login_required
 def testing(request):
