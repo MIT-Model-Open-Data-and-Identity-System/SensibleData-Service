7dcd710156e9916b72e8c25d9534eb81fda5e6db
Arek Stopczynski <arekquest@gmail.com>
2013-07-10 11:12:08 +0000

    added url for questionnaire connector

--- sensible_data_service/connectors/urls.py
+++ sensible_data_service/connectors/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-	 url(r'^connector_funf/', include('connectors.connector_funf.urls')),
+	 url(r'^connector_funf/?', include('connectors.connector_funf.urls')),
+	 url(r'^connector_questionnaire/?', include('connectors.connector_questionnaire.urls')),
 )
