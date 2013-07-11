dc4b0d3930ec1a39290fcdd9b1ecc0e08a80fe89
Arek Stopczynski <arekquest@gmail.com>
2013-07-11 09:07:47 +0000

    added refresh token url

--- sensible_data_service/connectors/connector_questionnaire/urls.py
+++ sensible_data_service/connectors/connector_questionnaire/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('connectors.connector_questionnaire',
 
          url(r'^auth/grant/', 'auth.grant'),
          url(r'^auth/token/', 'auth.token'),
+         url(r'^auth/refresh_token/', 'auth.refresh_token'),
 #         url(r'^auth/revoke/', 'auth.revoke'),
 #         url(r'^auth/sync/', 'auth.sync'),
 )
