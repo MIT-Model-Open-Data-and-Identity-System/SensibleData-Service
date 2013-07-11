5692767cadf4f059169e83f070a4edfcc5e09640
Arek Stopczynski <arekquest@gmail.com>
2013-07-11 09:09:04 +0000

    populating database

--- sensible_data_service/connectors/connector_questionnaire/connector_questionnaire.py
+++ sensible_data_service/connectors/connector_questionnaire/connector_questionnaire.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 from authorization_manager import authorization_manager
 import json
+from utils.database import Database
+import urllib
 
 def upload(request):
 
 	auth = authorization_manager.authenticate_token(request)
 	if 'error' in auth:
-		return HttpResponse(json.dumps(auth))
+		return HttpResponse(json.dumps(auth), status=401)
 	
 
-	#TODO process data
+	user = auth['user']
+	database = Database()
+	doc = urllib.unquote(request.REQUEST.get('doc'))
+	doc = json.loads(doc)
+	doc['user'] = user.username
+	doc_id = database.insert(doc, collection='questionnaire')
 
-	return HttpResponse(auth['user'].username)
+	return HttpResponse(json.dumps({'ok':str(doc_id)}), status=200)
