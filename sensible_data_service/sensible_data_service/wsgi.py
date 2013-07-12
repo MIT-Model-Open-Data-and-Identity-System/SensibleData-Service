bf9ece843ffb0c6d369803fcc6496fe344af8dc2
h0pbeat <arekquest@gmail.com>
2013-07-13 01:01:29 +0200

    wsgi for deployment

--- sensible_data_service/sensible_data_service/wsgi.py
+++ sensible_data_service/sensible_data_service/wsgi.py
@@ -16,20 +16,11 @@ framework.
 import os
 import sys
 
-sys.path.append('/home/arks/MODIS/Sensible-Data-Service/sensible_data_service')
-
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
+sys.path.append('/home/sensibleDTU/sensible-dtu-service/SensibleData-Service/sensible_data_service')
+sys.path.append('/home/sensibleDTU/sensible-dtu-service/SensibleData-Service')
+sys.path.append('/home/sensibleDTU/sensible-dtu-service')
 os.environ["DJANGO_SETTINGS_MODULE"] = "sensible_data_service.settings"
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sensible_data_service.settings")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
