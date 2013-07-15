3f226538ba432dfe7479f00952990a861d4de3aa
h0pbeat <arekquest@gmail.com>
2013-07-15 18:27:19 +0200

    named authorize url

--- sensible_data_service/authorization_manager/oauth2_urls.py
+++ sensible_data_service/authorization_manager/oauth2_urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('',
         (r'^missing_redirect_uri/?$',   'authorization_manager.oauth2.missing_redirect_uri'),
-        (r'^authorize/?$',              'authorization_manager.oauth2.authorize'),
+        url(r'^authorize/?$',              'authorization_manager.oauth2.authorize', name='oauth2_authorize'),
         (r'^token/?$',                  'oauth2app.token.handler'),
 )
