fda026c7e5bcfd560b7a0bb93f85fd007cdec915
h0pbeat <arekquest@gmail.com>
2013-07-17 16:51:28 +0000

    tweaked deploy for using ip and port

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 apt-get install python-pip
 pip install -r requirements.txt
 
-python sensible_data_service/manage.py runserver 166.78.249.214:8082
+python sensible_data_service/manage.py runserver $1:$2
