cb582e71403a4c4098359a7809bf091c2c0ab78e
h0pbeat <arekquest@gmail.com>
2013-07-17 16:57:39 +0000

    improved errors

--- sensible_data_service/platform_manager/platform_manager.py
+++ sensible_data_service/platform_manager/platform_manager.py
@@ -5,10 +5,10 @@ import json
 
 def authenticate(request):
 	token = request.REQUEST.get('access_token')
-        host = request.META['REMOTE_ADDR']
-	if not host in service_config.PLATFORM['ip_addr']: return {'error': 'ip address not authorized', 'response': json.dumps({'error': 'ip address not authorized'})}
+	host = request.META['REMOTE_ADDR']
+	if not host in service_config.PLATFORM['ip_addr']: return {'error': 'ip address not authorized', 'response': json.dumps({'error': 'ip address not authorized '+host})}
 
 	try: user = PlatformAccessToken.objects.get(token=token).user
-        except PlatformAccessToken.DoesNotExist: return {'error': 'no user found', 'response': json.dumps({'error': 'no user found'})}
+        except PlatformAccessToken.DoesNotExist: return {'error': 'user not found', 'response': json.dumps({'error': 'user not found'})}
 
 	return {'ok':'authenticated', 'user': user}
