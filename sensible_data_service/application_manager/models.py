b47e0afd78404375ce3d2bc552e66452f0c17d3c
h0pbeat <arekquest@gmail.com>
2013-07-18 21:04:13 +0000

    added device model to gcm registration

--- sensible_data_service/application_manager/models.py
+++ sensible_data_service/application_manager/models.py
@@ -35,7 +35,7 @@ class Application(models.Model):
 	description = models.TextField(null=True, blank=True)
 	connector_type = models.CharField(max_length=100)
 	client = models.ForeignKey(Client, null=True, blank=True)
-	url = models.URLField(null=True)
+	url = models.URLField(null=True, blank=True)
 	
 	def __unicode__(self):
 		return self.name+':'+self._id
@@ -43,10 +43,13 @@ class Application(models.Model):
 class Device(models.Model):
 	user = models.ForeignKey(User)
 	device_id = models.CharField(max_length=100, unique=True)
+
+	def __unicode__(self):
+		return self.device_id
 	
 
 class GcmRegistration(models.Model):
 	user = models.ForeignKey(User)
-	device_id = models.ForeignKey(Device)
+	device = models.ForeignKey(Device)
 	application = models.ForeignKey(Application, null=True)
 	gcm_id = models.CharField(max_length=256)
