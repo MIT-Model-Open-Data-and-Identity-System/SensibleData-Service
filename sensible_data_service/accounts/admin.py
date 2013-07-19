6629dac4f06385854066f11593d192b598b9c1eb
Arek Stopczynski <arekquest@gmail.com>
2013-07-19 16:20:11 +0200

    added user role

--- sensible_data_service/accounts/admin.py
+++ sensible_data_service/accounts/admin.py
@@ -2,15 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from accounts.models import Participant
+from accounts.models import Participant, UserRole
 
 class ParticipantInline(admin.StackedInline):
 	model = Participant
 	can_delete = True
 	verbose_name_plural = 'participant'
 
+class UserRoleInline(admin.StackedInline):
+	model = UserRole
+	can_delete = True
+
 class UserAdmin(UserAdmin):
-	inlines = (ParticipantInline, )
+	inlines = (ParticipantInline, UserRoleInline)
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
--- sensible_data_service/accounts/models.py
+++ sensible_data_service/accounts/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import User
 class Participant(models.Model):
 	user = models.OneToOneField(User)
 	status = models.CharField(max_length=100, blank=True)
+
+class Role(models.Model):
+	role = models.CharField(max_length=100, blank=True)
+	def __unicode__(self):
+		return self.role
+
+class UserRole(models.Model):
+	user = models.OneToOneField(User)
+	roles = models.ManyToManyField(Role)
