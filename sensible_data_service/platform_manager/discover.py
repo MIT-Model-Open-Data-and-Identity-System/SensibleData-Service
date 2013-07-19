2a9a5210abb1f4eee23c6c96227c30fb1496f8fc
Arek Stopczynski <arekquest@gmail.com>
2013-07-19 16:10:15 -0400

    service discovery

--- sensible_data_service/platform_manager/discover.py
+++ sensible_data_service/platform_manager/discover.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+import json
+from django.conf import settings
+from documents import get_documents
 
 def init(request):
-	return HttpResponse('hello')
+	response = {}
+	response['service_name'] = settings.SERVICE_NAME
+	response['service_desc_short'] = get_documents.getText(name='service_desc_short', lang='en').replace('\n','</br>')
+	response['service_desc_full'] = get_documents.getText(name='service_desc_full', lang='en').replace('\n','</br>')
+	return HttpResponse(json.dumps(response))
+
+def tos(request):
+	response = {}
+	response['service_tos'] = get_documents.getText(name='service_tos', lang='en').replace('\n','</br>')
+	response['service_tos_version'] = get_documents.getText(name='service_tos_version', lang='en').replace('\n','</br>')
+	return HttpResponse(json.dumps(response))
