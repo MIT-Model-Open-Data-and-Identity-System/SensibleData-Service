dde9928f8322297e37b2b20256a59d9585189a91
Arek Stopczynski <arekquest@gmail.com>
2013-07-30 23:00:35 +0200

    removed newline replacing when serving html/txt files

--- sensible_data_service/platform_manager/discover.py
+++ sensible_data_service/platform_manager/discover.py
@@ -6,12 +6,12 @@ from documents import get_documents
 def init(request):
 	response = {}
 	response['service_name'] = settings.SERVICE_NAME
-	response['service_desc_short'] = get_documents.getText(name='service_desc_short', lang='en').replace('\n','</br>')
-	response['service_desc_full'] = get_documents.getText(name='service_desc_full', lang='en').replace('\n','</br>')
+	response['service_desc_short'] = get_documents.getText(name='service_desc_short', lang='en').replace('\n','')
+	response['service_desc_full'] = get_documents.getText(name='service_desc_full', lang='en').replace('\n','')
 	return HttpResponse(json.dumps(response))
 
 def tos(request):
 	response = {}
-	response['service_tos'] = get_documents.getText(name='service_tos', lang='en').replace('\n','</br>')
-	response['service_tos_version'] = get_documents.getText(name='service_tos_version', lang='en').replace('\n','</br>')
+	response['service_tos'] = get_documents.getText(name='service_tos', lang='da').replace('\n','')
+	response['service_tos_version'] = get_documents.getText(name='service_tos_version', lang='da').replace('\n','')
 	return HttpResponse(json.dumps(response))
