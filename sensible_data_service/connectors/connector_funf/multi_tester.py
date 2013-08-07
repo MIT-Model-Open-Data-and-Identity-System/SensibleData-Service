798da5176e2e0bd6121c3fef1d8db90ae4996b42
sapiezynski <piotr@sapiezynski.eu>
2013-08-07 12:33:49 +0200

    Updated the informed consent with grammar mistakes corrections

--- sensible_data_service/connectors/connector_funf/multi_tester.py
+++ sensible_data_service/connectors/connector_funf/multi_tester.py
@@ -2,7 +2,7 @@ from subprocess import Popen
 from time import sleep
 
 # multithreaded tester
-for i in xrange(200):
+for i in xrange(500):
 	Popen(['python','tester.py','43082bbb-0e2f-49cb-9c32-a425d262b4c9_1375626293937_mainPipeline.db','> output_' + str(i)]);
 	sleep(0.05)
 
