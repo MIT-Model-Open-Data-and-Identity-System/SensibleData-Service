2f42edcd7e32698a3ce4a39e3bd362de79dc6c46
sapiezynski <piotr@sapiezynski.eu>
2013-08-09 16:37:51 +0200

    not the commit you are looking for

--- sensible_data_service/connectors/connector_funf/multi_tester.py
+++
@@ -1,8 +0,0 @@
-from subprocess import Popen
-from time import sleep
-
-# multithreaded tester
-for i in xrange(500):
-	Popen(['python','tester.py','43082bbb-0e2f-49cb-9c32-a425d262b4c9_1375626293937_mainPipeline.db','> output_' + str(i)]);
-	sleep(0.05)
-
