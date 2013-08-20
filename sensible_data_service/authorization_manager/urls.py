a425783ae03d3266827371586b0e6c46cd115ed4
h0pbeat <arekquest@gmail.com>
2013-08-20 19:22:35 +0000

    added authorization manager connector raw endpoint

--- sensible_data_service/authorization_manager/urls.py
+++ sensible_data_service/authorization_manager/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^connector_funf/?', include('connectors.connector_funf.urls')),
+	url(r'^connector_raw/?', include('connectors.connector_raw.urls')),
 	url(r'^connector_questionnaire/?', include('connectors.connector_questionnaire.urls')),
 	url(r'^connector_facebook/?', include('connectors.connector_facebook.urls')),
 	url(r'^oauth2/?', include('authorization_manager.oauth2_urls')),
