73282fd4e1df76419280787475212ab83d7336bc
h0pbeat <arekquest@gmail.com>
2013-08-27 14:26:34 +0000

    description of the app and no researcher scopes shown to the user

--- sensible_data_service/render/researcher.py
+++ sensible_data_service/render/researcher.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from accounts.models import UserRole
 from application_manager import application_manager
 import authorization_manager.authorization_manager
+import time
 
 @login_required
 def researcher(request):
@@ -17,12 +18,17 @@ def researcher(request):
 		if not application.connector_type == 'data_viewer': continue
 		values[application.name] = {}
 		values[application.name]['uri'] = application.url
+		values[application.name]['description'] = application.description
 		application_scopes = application_manager.getApplicationScopes(application)
 		values[application.name]['scopes'] = {}
 		for scope in application_scopes:
+			if not 'researcher' in scope.scope: continue
 			auth = authorization_manager.authorization_manager.getAuthorization(request.user, scope, application)
+			auth = [x for x in auth if x.access_token.expire > time.time()]
+
 			values[application.name]['scopes'][scope.scope] = {}
 			values[application.name]['scopes'][scope.scope]['authorized'] = 1 if len(auth)>0 else 0
+			values[application.name]['scopes'][scope.scope]['authorization'] = auth 
 			values[application.name]['scopes'][scope.scope]['description'] = scope.description
 			values[application.name]['scopes'][scope.scope]['description_extra'] = scope.description_extra
 			values[application.name]['scopes'][scope.scope]['auth_url'] = authorization_manager.authorization_manager.buildAuthUrl(scope.connector, application)
