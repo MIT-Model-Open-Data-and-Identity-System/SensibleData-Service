08e4e0d0ebb79a0088e3059b1fcde6cc939617b1
h0pbeat <arekquest@gmail.com>
2013-10-04 14:41:52 +0000

    added file only log

--- sensible_data_service/utils/audit.py
+++ sensible_data_service/utils/audit.py
@@ -19,24 +19,27 @@ class Audit:
 		doc['type']=typ
 		log.log(severity,json.dumps(doc))
 
-	def decision(self, severity,typ,tag,doc):
+	def shouldCopyToFile(self, severity,typ,tag,doc):
 		if severity=='ERROR': return True
 		else: return False	
 
-	def log(self, severity, typ, tag, doc):
-		doc['TIMESTAMP']=str(datetime.datetime.now()) 
-		doc['tag']= tag
-		doc['severity']= severity
-		coll = self.db[typ]
-		doc_id=coll.insert(doc, continue_on_error=True)
+	def log(self, severity, typ, tag, doc, onlyfile):
+		if onlyfile:
+			 self.copyToFile(severity, typ, doc)
+		else:
+			doc['TIMESTAMP']=str(datetime.datetime.now()) 
+			doc['tag']= tag
+			doc['severity']= severity
+			coll = self.db[typ]
+			doc_id=coll.insert(doc, continue_on_error=True)
 
-		if self.decision(severity,typ,tag,doc): self.copyToFile(severity, typ, doc)
+			if self.shouldCopyToFile(severity,typ,tag,doc): self.copyToFile(severity, typ, doc)
 
-	def d(self, typ, tag, doc):
-		self.log('DEBUG', typ, tag, doc)
+	def d(self, typ, tag, doc, onlyfile=False):
+		self.log('DEBUG', typ, tag, doc, onlyfile)
 		
-	def w(self, typ, tag, doc):
-		self.log('WARNING', typ, tag, doc)
+	def w(self, typ, tag, doc, onlyfile=False):
+		self.log('WARNING', typ, tag, doc, onlyfile)
 
-	def e(self, typ, tag, doc):
-		self.log('ERROR', typ, tag, doc)
+	def e(self, typ, tag, doc, onlyfile=False):
+		self.log('ERROR', typ, tag, doc, onlyfile)
