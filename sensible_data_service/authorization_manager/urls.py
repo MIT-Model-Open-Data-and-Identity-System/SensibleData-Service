3e6edb4e367e4c920747ee018bb4e05ec96b9bdf
h0pbeat <arekquest@gmail.com>
2013-10-08 16:10:48 +0000

    added sbs2 url

--- sensible_data_service/authorization_manager/urls.py
+++ sensible_data_service/authorization_manager/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 	url(r'^connector_funf/?', include('connectors.connector_funf.urls')),
+	url(r'^connector_sbs2/?', include('connectors.connector_sbs2.urls')),
 	url(r'^connector_raw/?', include('connectors.connector_raw.urls')),
 	url(r'^connector_questionnaire/?', include('connectors.connector_questionnaire.urls')),
 	url(r'^connector_facebook/?', include('connectors.connector_facebook.urls')),
