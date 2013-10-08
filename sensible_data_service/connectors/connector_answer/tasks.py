640451a24403e1553b7dcb1cc344ecbe7fea00fa
h0pbeat <arekquest@gmail.com>
2013-10-08 20:32:35 +0000

    added locks for task

--- sensible_data_service/connectors/connector_answer/tasks.py
+++ sensible_data_service/connectors/connector_answer/tasks.py
@@ -1,6 +1,20 @@
 from celery import task
-from questions import test_question
+from questions import statistics_question
+from django.core.cache import cache
+import time
 
 @task()
-def add(x,y):
-	return test_question.run()
+def calculateStatistics(collections):
+	question = statistics_question
+
+	LOCK_EXPIRE = question.LOCK_EXPIRE
+	lock_id = 'lock-%s'%question.NAME
+	acquire_lock = lambda: cache.add(lock_id, "true", LOCK_EXPIRE)
+	release_lock = lambda: cache.delete(lock_id)
+	if acquire_lock():
+		try:
+			for collection in collections: question.run(collection)
+		finally:
+			release_lock()
+		return True
+	return False
