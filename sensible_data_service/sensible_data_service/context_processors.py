b740e0e5e80f147cb550643ae461c40b532611d3
Lasse Valentini Jensen <lasse.valentini@gmail.com>
2013-10-14 11:20:28 +0000

    version now read from VERSION file

--- sensible_data_service/sensible_data_service/context_processors.py
+++ sensible_data_service/sensible_data_service/context_processors.py
@@ -1,4 +1,10 @@
 from sensible_data_service import settings as service_settings
+import os
 
 def service(request):
-    return {'service':{'name':service_settings.SERVICE_NAME}}
+    path = os.path.abspath(os.path.join(service_settings.ROOT_DIR,"..","VERSION"))
+    with open(path) as f:
+        version = f.read()
+    is_devel = '-devel' in version
+
+    return {'service':{'name':service_settings.SERVICE_NAME, 'version':version, 'is_devel':is_devel}}
