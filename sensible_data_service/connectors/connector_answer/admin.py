4523520d9cdb84bd1f063c80307ce0c1e6389bf1
h0pbeat <arekquest@gmail.com>
2013-10-17 13:18:38 +0000

    removed deleted questions.models import

--- sensible_data_service/connectors/connector_answer/admin.py
+++ sensible_data_service/connectors/connector_answer/admin.py
@@ -8,3 +8,8 @@ class ConnectorAnswerAdmin(admin.ModelAdmin):
 		verbose_name = 'connector'
 
 admin.site.register(ConnectorAnswer, ConnectorAnswerAdmin)
+
+class ConnectorAnswerEndpointAdmin(admin.ModelAdmin):
+	list_display = ('question','answer', 'active')
+
+admin.site.register(ConnectorAnswerEndpoint, ConnectorAnswerEndpointAdmin)
