531764d1c8f98784e028e91f4c587546ba5903be
h0pbeat <arekquest@gmail.com>
2013-10-17 13:21:43 +0000

    added connector answer endpoint

--- sensible_data_service/connectors/connector_answer/models.py
+++ sensible_data_service/connectors/connector_answer/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from connectors.models import *
-
+from connectors.models import Scope, Connector
 
 class ConnectorAnswer(Connector):
 	class Meta:
 		app_label = 'connectors'
+
+class ConnectorAnswerEndpoint(models.Model):
+	question = models.CharField(max_length=100, db_index=True, help_text="Associated question name")
+	answer = models.CharField(unique=True, max_length=100, db_index=True, help_text="Endpoint name")
+	scopes = models.ManyToManyField(Scope)
+	active = models.BooleanField(default=False)
