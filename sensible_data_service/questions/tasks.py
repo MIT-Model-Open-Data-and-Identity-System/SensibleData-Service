e7af39eadb00f82b3afbf5187e48b32f8b366576
h0pbeat <arekquest@gmail.com>
2013-10-17 13:15:14 +0000

    fully generic task runner

--- sensible_data_service/questions/tasks.py
+++ sensible_data_service/questions/tasks.py
@@ -2,15 +2,18 @@ from celery import task
 from django.core.cache import cache
 import time
 
+
 @task()
-def run(question, name):
-	LOCK_EXPIRE = 60*60 #max time in sec after which the task will be considered zombie
-	lock_id = 'lock-%s'%name
+def run(question, LOCK_EXPIRE=60*60):
+	exec('from questions.available_questions import ' + question) in globals()
+	question = eval(question)
+	lock_id = 'lock-%s'%question.NAME
 	acquire_lock = lambda: cache.add(lock_id, "true", LOCK_EXPIRE)
 	release_lock = lambda: cache.delete(lock_id)
+	print question.NAME
 	if acquire_lock():
 		try:
-			question()
+			question.run()
 		finally:
 			release_lock()
 		return True
