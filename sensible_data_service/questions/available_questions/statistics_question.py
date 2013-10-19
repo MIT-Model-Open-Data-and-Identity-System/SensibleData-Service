12176cd5b1c8c53e07af4775bc8817c5904d91bc
h0pbeat <arekquest@gmail.com>
2013-10-19 17:29:12 +0000

    added proper handling of user roles

--- sensible_data_service/questions/available_questions/statistics_question.py
+++ sensible_data_service/questions/available_questions/statistics_question.py
@@ -65,14 +65,24 @@ def findPreviousMax(db, collection):
 def data_stats(request, user, scopes, users_to_return, user_roles, own_data):
 	db = database.Database()
 
+	roles_to_use = []
+	if own_data and 'researcher' in user_roles: roles_to_use = ['researcher']
+	if own_data and 'developer' in user_roles: roles_to_use = ['developer']
+
+	collections_to_use = request.GET.get('collections','')
+	if collections_to_use == '': collections_to_use = [x for x in COLLECTIONS]
+	else: collections_to_use = collections_to_use.split(',')
+
+	collections_to_use = [x for x in set(collections_to_use).intersection(set(COLLECTIONS))]
+
 	query = {'$query':{}}
 	if not 'all' in users_to_return:
 		query['$query']['_id'] = {'$in':users_to_return}
 
 	results = {}
-	for collection in COLLECTIONS:
+	for collection in collections_to_use:
 		results[collection] = {}
-		for x in db.getDocuments(query=query, collection=NAME+'_'+collection, roles=user_roles): 
+		for x in db.getDocuments(query=query, collection=NAME+'_'+collection, roles=roles_to_use): 
 			results[collection][x['_id']] = int(x['value']['count'])
 
 
