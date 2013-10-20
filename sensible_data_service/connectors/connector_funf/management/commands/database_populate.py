40f28edc350515ffed7825115b16ce50e67687e6
h0pbeat <arekquest@gmail.com>
2013-10-20 11:38:44 +0000

    moved commands to audit app

--- sensible_data_service/connectors/connector_funf/management/commands/database_populate.py
+++ sensible_data_service/connectors/connector_funf/management/commands/database_populate.py
@@ -1,12 +1,12 @@
 from connectors.connector_funf.database_single_population import *
-from utils.log import log
+from sensible_auditor import audit
 
 from django.core.management.base import NoArgsCommand
 
 class Command(NoArgsCommand):
 	def handle_noargs(self, **options):
 		try:
-			log('Debug', 'Running database population script')
+			audit.Audit().d(type='connector_funf', tag='population', doc={'message': 'Running database population script'})
 			load_files()
 		except Exception as e:
-			log('Error', 'Exception thrown from database population script: ' + str(e))
+			audit.Audit().e(type='connector_funf', tag='population', doc={'message': 'Exception thrown from database population script: ' + str(e)})
