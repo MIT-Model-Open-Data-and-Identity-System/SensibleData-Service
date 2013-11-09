1b717702246874e2dec65c3123702ff108fe312e
h0pbeat <arekquest@gmail.com>
2013-11-09 19:05:05 +0000

    added device_inventory

---
+++ sensible_data_service/connectors/connector_funf/device_inventory.py
@@ -0,0 +1,28 @@
+from utils import database
+from collections import defaultdict
+import json
+
+class DeviceInventory(object):
+	
+	db = None
+	mapping = defaultdict(list)
+
+	def __init__(self):
+		self.db = database.Database()
+		for inventory in self.db.getDocuments(query={}, collection = 'device_inventory'):
+			self.mapping[inventory['a_bt_mac']].append(inventory)
+
+
+	def mapBtToUser(self, bt_mac, timestamp, use_mac_if_empty=True):
+		devices = self.mapping[bt_mac]
+		if len(devices) == 0: 
+			if use_mac_if_empty: return bt_mac
+			else: return None
+		if len(devices) == 1: return devices[0]['user']
+
+		for d in devices:
+			if d['start'] <= timestamp and d['end'] > timestamp:
+				return d['user']
+		return bt_mac	
+
+		
