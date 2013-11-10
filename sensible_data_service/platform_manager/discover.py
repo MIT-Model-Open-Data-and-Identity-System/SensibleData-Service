993f5d31d49a502927578ff76d1ceac1ebfe340f
root <root@ip-172-31-42-85.eu-west-1.compute.internal>
2013-11-10 16:51:07 +0000

    changed the language of informed consent to the language of user's choice instead of hardcoded danish

--- sensible_data_service/platform_manager/discover.py
+++ sensible_data_service/platform_manager/discover.py
@@ -6,12 +6,12 @@ from documents import get_documents
 def init(request):
 	response = {}
 	response['service_name'] = settings.SERVICE_NAME
-	response['service_desc_short'] = get_documents.getText(name='service_desc_short', lang='da').replace('\n','')
-	response['service_desc_full'] = get_documents.getText(name='service_desc_full', lang='da').replace('\n','')
+	response['service_desc_short'] = get_documents.getText(name='service_desc_short', lang=request.GET.get("language")).replace('\n','')
+	response['service_desc_full'] = get_documents.getText(name='service_desc_full', lang=request.GET.get("language")).replace('\n','')
 	return HttpResponse(json.dumps(response))
 
 def informed_consent(request):
 	response = {}
-	response['service_informed_consent'] = get_documents.getText(name='service_informed_consent', lang='da').replace('\n','')
-	response['service_informed_consent_version'] = get_documents.getText(name='service_informed_consent_version', lang='da').replace('\n','')
+	response['service_informed_consent'] = get_documents.getText(name='service_informed_consent', lang=request.GET.get("language")).replace('\n','')
+	response['service_informed_consent_version'] = get_documents.getText(name='service_informed_consent_version', lang=request.GET.get("language")).replace('\n','')
 	return HttpResponse(json.dumps(response))
