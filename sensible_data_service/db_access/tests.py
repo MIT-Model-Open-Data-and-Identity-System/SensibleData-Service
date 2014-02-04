57f017a766c7fff3ace561893fb1045f5693156b
radugatej <radu.gatej@gmail.com>
2014-02-04 18:38:52 +0100

    Added connection pooling and user role support

---
+++ sensible_data_service/db_access/tests.py
@@ -0,0 +1,17 @@
+"""
+This file demonstrates writing tests using the unittest module. These will pass
+when you run "manage.py test".
+
+Replace this with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+from sensible_data_service.db_access.mysql_wrapper import Wrapper
+
+
+class TestWrapper(TestCase):
+	def test_get_db_connection(self):
+		test_database = "testdb"
+		wrapper = Wrapper()
+		conn = wrapper.get_db_connection_for_probe(test_database)
+		self.fail("Finish the test")
