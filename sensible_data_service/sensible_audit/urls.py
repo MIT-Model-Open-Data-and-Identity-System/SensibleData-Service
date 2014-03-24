717d1beb758eb2f1d8044b984135c42434301031
radugatej <Looptroop88>
2014-03-24 12:02:30 +0100

    Removed audit dasboard urls (not ready for production)

--- sensible_data_service/sensible_audit/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-	url(r'^dashboard/?$', 'sensible_audit.audit.dashboard'),
-	url(r'^accesses/?$', 'sensible_audit.audit.accesses'),
-)
