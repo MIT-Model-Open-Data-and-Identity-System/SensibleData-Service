03f05dd7d48f6e0ab07e73cd1c70a204b10e205b
radugatej <radu.gatej@gmail.com>
2014-03-31 09:09:02 +0200

    Removed debug output

--- sensible_data_service/db_access/management/commands/populate_file_database.py
+++ sensible_data_service/db_access/management/commands/populate_file_database.py
@@ -6,8 +6,6 @@ from sensible_data_service import settings
 class Command(BaseCommand):
 
 	def handle(self, *args, **options):
-		print "before rsync"
 		os.system(
 	"rsync --remove-source-files -aze ssh --exclude 'temp' " + settings.FILESYSTEM_DATABASE["LOCAL_DIR"] + " " +
 	settings.FILESYSTEM_DATABASE["REMOTE_HOST"] + ":" + settings.FILESYSTEM_DATABASE["REMOTE_HOST_DIR"])
-		print "after rsync"
