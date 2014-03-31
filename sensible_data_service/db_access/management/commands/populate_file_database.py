2a2a0f807c20d175b2e2b2dfd27153e7b4fa6a30
radugatej <radu.gatej@gmail.com>
2014-03-31 09:08:18 +0200

    Modified rsync command to remove unneeded verbosity

--- sensible_data_service/db_access/management/commands/populate_file_database.py
+++ sensible_data_service/db_access/management/commands/populate_file_database.py
@@ -8,6 +8,6 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		print "before rsync"
 		os.system(
-	"rsync --remove-source-files -azvhe ssh --exclude 'temp' " + settings.FILESYSTEM_DATABASE["LOCAL_DIR"] + " " +
+	"rsync --remove-source-files -aze ssh --exclude 'temp' " + settings.FILESYSTEM_DATABASE["LOCAL_DIR"] + " " +
 	settings.FILESYSTEM_DATABASE["REMOTE_HOST"] + ":" + settings.FILESYSTEM_DATABASE["REMOTE_HOST_DIR"])
 		print "after rsync"
