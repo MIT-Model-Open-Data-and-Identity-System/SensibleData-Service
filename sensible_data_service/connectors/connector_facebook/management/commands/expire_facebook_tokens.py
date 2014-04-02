f8f3187201d47dc3b57752a416f5a55675425604
radugatej <radu.gatej@gmail.com>
2014-04-02 18:35:27 +0200

    MySQL migration part 2: Updated retrieval api - facebook tokens

--- sensible_data_service/connectors/connector_facebook/management/commands/expire_facebook_tokens.py
+++ sensible_data_service/connectors/connector_facebook/management/commands/expire_facebook_tokens.py
@@ -1,24 +1,15 @@
 from django.core.management.base import NoArgsCommand
-from authorization_manager import authorization_manager
 from authorization_manager.models import Authorization
-from application_manager.models import Application, GcmRegistration
-from collections import defaultdict
+from application_manager.models import Application
 import json
 import time
-from utils import database
-from application_manager import gcm_server
-from connectors.connector_facebook import auth
 
 class Command(NoArgsCommand):
 	def handle_noargs(self, **options):
-		user_auths = defaultdict(lambda: defaultdict(int))
 		authorizations = Authorization.objects.filter(application=Application.objects.get(connector_type='facebook_in'), active=True)
-		db = database.Database()
-		inventory = list()
 		now = time.time()
-		for a in authorizations:
-			user = a.user
-			expires_at = json.loads(a.payload)['expires_at']
+		for authorization in authorizations:
+			expires_at = json.loads(authorization.payload)['expires_at']
 			if expires_at < now:
-				a.active = False
-				a.save()
+				authorization.active = False
+				authorization.save()
