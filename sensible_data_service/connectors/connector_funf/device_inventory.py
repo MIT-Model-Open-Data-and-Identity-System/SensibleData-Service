9eefc7bea4dcecfb1811b85d421b12b8affae97e
radugatej <radu.gatej@gmail.com>
2014-04-02 18:26:33 +0200

    MySQL migration part 2: retrieval API - device inventory

--- sensible_data_service/connectors/connector_funf/device_inventory.py
+++ sensible_data_service/connectors/connector_funf/device_inventory.py
@@ -1,4 +1,5 @@
-from utils import database
+from db_access.named_queries import NAMED_QUERIES
+from utils import db_wrapper
 from collections import defaultdict
 import json
 
@@ -8,8 +9,8 @@ class DeviceInventory(object):
 
 	def __init__(self):
 		self.mapping = defaultdict(list)
-		self.db = database.Database()
-		for inventory in self.db.getDocuments(query={}, collection = 'device_inventory'):
+		self.db = db_wrapper.DatabaseHelper()
+		for inventory in self.db.execute_named_query(NAMED_QUERIES["get_device_inventory"], None):
 			self.mapping[inventory['a_bt_mac']].append(inventory)
 
 
