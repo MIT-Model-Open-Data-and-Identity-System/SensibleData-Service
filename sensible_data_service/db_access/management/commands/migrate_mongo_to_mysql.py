1b5a3b98ade3ea170d50f59ed84bed2257bc1e31
h0pbeat <arekquest@gmail.com>
2014-04-04 19:35:51 +0200

    Fixed bugs in mysql migration command and added method for inserting to mysql using given connection

--- sensible_data_service/db_access/management/commands/migrate_mongo_to_mysql.py
+++ sensible_data_service/db_access/management/commands/migrate_mongo_to_mysql.py
@@ -6,8 +6,9 @@ from db_access.filesystem_wrapper import FileSystemWrapper
 from db_access.mysql_wrapper import DBWrapper
 from utils import SECURE_settings
 from sensible_audit import audit
+import sys
 
-
+BULK_SIZE = 200
 class Command(BaseCommand):
 	# def __init__(self):
 	# 	self.log = audit.getLogger(__name__)
@@ -20,31 +21,48 @@ class Command(BaseCommand):
 		if len(args) > 1:
 			if args[1] not in roles:
 				raise BaseException("incorrect role")
+			else:
+				role = args[1]
 
-		username = SECURE_settings.DATA_DATABASE["username"]
-		password = SECURE_settings.DATA_DATABASE["password"]
+		username = "radugatej"#SECURE_settings.DATA_DATABASE["username"]
+		password = "kdbgaCQgCPJ4uFxLA6RA"#SECURE_settings.DATA_DATABASE["password"]
 		host = settings.DATA_DATABASE["nodes"][0]
-
-		client = MongoClient('mongodb://%s:%s@%s/admin'%(username, password, host), ssl=True)
+		database = "admin"
+		client = MongoClient('mongodb://%s:%s@%s/%s'%(username, password, host, database), ssl=True)
+		print "connected"
 		collection_name = probe + "_" + role if not role == None else probe
-		collection = client[settings.DATA_DATABASE["available_databases"]][collection_name]
-
+		database_name = settings.DATA_DATABASE["available_databases"].get(probe, "sensibledtu_1k")
+		collection = client[database_name][collection_name]
+		print "after collection"
+		sys.stdout.flush()
 		counter = 0
 		mysql_wrapper = DBWrapper()
-		filestorage_wrapper = FileSystemWrapper()
-
+		#filestorage_wrapper = FileSystemWrapper()
+		connection = mysql_wrapper.get_write_db_connection_for_probe(probe)
 		payload = []
+		print collection.find().count()
 		for document in collection.find():
 			try:
 				payload += json_to_csv.json_to_csv(document, probe)
-				filestorage_wrapper.insert([document], probe, role)
+				#filestorage_wrapper.insert([document], probe, role)
 				counter += 1
-			except Exception, e: print repr(e)
+			except Exception, e: 
+				print repr(e)
+				sys.stdout.flush()
 
-			if counter % 1000 == 0:
+			if counter % BULK_SIZE == 0:
 				try:
-					mysql_wrapper.insert(payload, probe, role)
+					mysql_wrapper.insert_for_connection(connection, payload, probe, role)
 					print "comitted"
 				except Exception, e:
 					print repr(e)# self.log.e({'type': 'MYSQL', 'tag': 'insert', 'exception': str(e)})
+					sys.stdout.flush()
 				payload = []
+		if len(payload) > 0:
+			try:
+				mysql_wrapper.insert_for_connection(connection, payload, probe, role)
+                                print "comitted"
+                        except Exception, e:
+                                print repr(e)# self.log.e({'type': 'MYSQL', 'tag': 'insert', 'exception': str(e)})
+		connection.commit()
+		connection.close()
