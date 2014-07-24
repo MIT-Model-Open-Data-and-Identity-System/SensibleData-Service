1740371c4f6032ed676917e5a254eaa193c6438b
radugatej <radu.gatej@gmail.com>
2014-07-25 00:15:44 +0200

    EpidemicProbe data ca now populate. There is also an endpoint for retrieval

--- sensible_data_service/db_access/PROBE_SETTINGS.py
+++ sensible_data_service/db_access/PROBE_SETTINGS.py
@@ -14,5 +14,6 @@ UNIQUE_FIELDS = {
 	"edu_mit_media_funf_probe_builtin_SMSProbe": ["type", "body", "address", "timestamp", "user"],
 	"edu_mit_media_funf_probe_builtin_TimeOffsetProbe": ["time_offset", "timestamp", "user"],
 	"edu_mit_media_funf_probe_builtin_WifiProbe": ["bssid", "level", "timestamp", "user"],
-	"edu_mit_media_funf_probe_builtin_ExperienceSamplingProbe": []
+	"edu_mit_media_funf_probe_builtin_ExperienceSamplingProbe": [],
+	"edu_mit_media_funf_probe_builtin_EpidemicProbe": []
 }
