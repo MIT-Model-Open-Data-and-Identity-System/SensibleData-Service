0b92bda4cb842dcdc140110684487e71461ab8d1
radugatej <radu.gatej@gmail.com>
2014-09-21 18:08:07 +0200

    Added question for rewarding high data quality with movie tickets

--- sensible_data_service/questions/available_questions/quality_prize_question.py
+++ sensible_data_service/questions/available_questions/quality_prize_question.py
@@ -2,8 +2,10 @@ import datetime
 from django.contrib.auth.models import User
 from questions.available_questions.data_quality_question import get_quality_for_users_and_period
 from questions.models import QualityPrizeWinners, PrizeTicket
+from sensible_audit import audit
 
 NAME = 'quality_prize_question'
+log = audit.getLogger(__name__)
 
 def run():
 	today = datetime.datetime.now()
@@ -18,7 +20,7 @@ def run():
 		used_prizes = QualityPrizeWinners.objects.values_list('prize', flat=True)
 		prizes = PrizeTicket.objects.order_by('?').exclude(id__in=used_prizes)
 		if len(prizes) == 0:
-			print "No more prizes!"
+			log.debug({"tag": "question", "type": "quality_prize", "message": "No more prizes available"})
 			return
 
 		quality_prize_winner = QualityPrizeWinners(user=winner['user'], start_timestamp=start_date, end_timestamp=end_date, quality=winner['quality'], prize=prizes[0])
