1930556b4d0f43bbfabf9f8cb3c645ab34a2b67b
radugatej <radu.gatej@gmail.com>
2014-12-05 01:36:10 +0100

    Added models for static time variable metadata as well as functions for accessing them

---
+++ sensible_data_service/user_metadata/metadata.py
@@ -0,0 +1,71 @@
+from collections import defaultdict
+from django.db.models import get_model
+from django.forms import model_to_dict
+from user_metadata.models import StaticMetadata
+
+
+STATIC_METADATA = ['facebook_id', 'gender', 'starting_year']
+DYNAMIC_METADATA = {'phone_number': 'UserPhoneNumber', 'study_line': 'UserStudyLine', 'status': 'UserStatus'}
+
+
+def get_metadata_for_users(users, timestamp, metadata_attributes=None):
+	static_attributes = [attribute for attribute in metadata_attributes if attribute in STATIC_METADATA]
+	dynamic_attributes = [attribute for attribute in metadata_attributes if attribute in DYNAMIC_METADATA]
+	static_metadata = get_static_metadata_for_users(users, static_attributes)
+	dynamic_metadata = get_dynamic_metadata_for_users(users, timestamp, dynamic_attributes)
+	metadata_dict = {}
+	for user in set(static_metadata.keys()).union(set(dynamic_metadata.keys())):
+		metadata_dict[user] = dict(static_metadata.get(user, {}).items() + dynamic_metadata.get(user, {}).items())
+
+	return metadata_dict
+
+
+def get_static_metadata_for_users(users, static_attributes):
+	metadata_dict = {}
+	static_attributes.append("user")
+	metadata_queryset = StaticMetadata.objects.filter(user__in=users).only(*static_attributes)
+	for metadata_result in metadata_queryset:
+		metadata_dict[metadata_result.user] = model_to_dict(metadata_result)
+	return metadata_dict
+
+
+def get_dynamic_metadata_for_users(users, timestamp, dynamic_attributes):
+	metadata_dict = {}
+	for attribute in dynamic_attributes:
+		metadata_queryset = get_model('user_metadata', DYNAMIC_METADATA[attribute]).objects.filter(user__in=users, start_timestamp__gte=timestamp, end_timestamp__lt=timestamp).defer("id", "start_timestamp", "end_timestamp")
+		for metadata_result in metadata_queryset:
+			metadata_dict[metadata_result.user] = model_to_dict(metadata_result)
+	return metadata_dict
+
+
+class CachedMetadata(object):
+	def __init__(self):
+		self.static_metadata_type = None
+		self.dynamic_metadata_type = None
+		self.static_metadata = {}
+		self.dynamic_metadata = defaultdict(list)
+
+	def get_static_metadata(self, user, metadata_type):
+		if metadata_type != self.static_metadata_type or len(self.static_metadata) == 0:
+			metadata_queryset = StaticMetadata.objects.filter(user__in=user).only(metadata_type)
+			for metadata_result in metadata_queryset:
+				self.static_metadata[metadata_result.user] = model_to_dict(metadata_result)
+
+		return self.static_metadata.get(user, {}).get(metadata_type, "")
+
+	def get_dynamic_metadata(self, user, metadata_type, timestamp):
+		if metadata_type != self.dynamic_metadata_type or len(self.dynamic_metadata) == 0:
+			metadata_queryset = get_model('user_metadata', DYNAMIC_METADATA[metadata_type]).objects.filter(user__in=user).defer("id")
+			for metadata_result in metadata_queryset:
+				self.dynamic_metadata[metadata_result.user].append(model_to_dict(metadata_result))
+
+		for metadata in self.dynamic_metadata[user]:
+			if metadata["start_timestamp"] <= timestamp <= metadata["end_timestamp"]:
+				return metadata.get(metadata_type, "")
+
+		return ""
+
+
+
+
+
--- sensible_data_service/user_metadata/models.py
+++ sensible_data_service/user_metadata/models.py
@@ -2,16 +2,12 @@ from django.db import models
 
 
 class TimeVariableMetadata(models.Model):
+	user = models.CharField(max_length=60)
 	start_timestamp = models.DateTimeField(null=True)
 	end_timestamp = models.DateTimeField(null=True)
 
-
-class StatusType(models.Model):
-	status_label = models.CharField(max_length=100)
-
-
-class UserStatus(TimeVariableMetadata):
-	status = models.ManyToManyField(StatusType)
+	class Meta:
+		abstract = True
 
 
 class UserPhoneNumber(TimeVariableMetadata):
@@ -23,12 +19,11 @@ class StudyLineType(models.Model):
 
 
 class UserStudyLine(TimeVariableMetadata):
-	study_line = models.ManyToOneRel(StudyLineType, "study_line_name")
+	study_line = models.ForeignKey('StudyLineType')
 
 
-class UserMetadata(models.Model):
-	user = models.CharField(max_length=100)
+class StaticMetadata(models.Model):
+	user = models.CharField(max_length=60)
 	facebook_id = models.CharField(max_length=100, null=True)
-	study_line = models.ManyToManyField(UserStudyLine, null=True)
-	current_status = models.ManyToManyField(UserStatus)
-	phone_number = models.ManyToManyField(UserPhoneNumber, null=True)
+	gender = models.CharField(max_length=1)
+	starting_year = models.IntegerField()
