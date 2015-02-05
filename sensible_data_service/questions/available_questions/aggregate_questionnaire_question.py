5d12d9dcd538b10c4384e4e44dba5c37526641ed
Radu Gatej <radu.gatej@gmail.com>
2015-02-05 15:40:00 +0100

    Aggregate questionnaire question can now return in json format (cherry picked from commit e2be95c) (cherry picked from commit 530f19f)

--- sensible_data_service/questions/available_questions/aggregate_questionnaire_question.py
+++ sensible_data_service/questions/available_questions/aggregate_questionnaire_question.py
@@ -1,5 +1,7 @@
 import json
+import time
 from db_access.named_queries.named_queries import NAMED_QUERIES
+from django.contrib.auth.models import User
 
 NAME = "aggregate_questionnaire_question"
 
@@ -12,21 +14,29 @@ def get_aggregated_questionnaire_data(request, user, scopes, users_to_return, us
 	if not form_version:
 		return {"status": {"code": 400, "message": "Please select a questionnaire version"}}
 	if "all" in users_to_return:
-		users_to_return = [x['user'] for x in
-						   db_helper.execute_named_query(NAMED_QUERIES["get_unique_users_in_device_inventory"], None)]
+		users = User.objects.all()
+                users_to_return = [user.username for user in users if not hasattr(user, "userrole")]
 	response = []
-	header = ['user']
-	header.extend([doc['variable_name'] for doc in
+	all_variable_names = ['user', 'timestamp']
+	all_variable_names.extend([doc['variable_name'] for doc in
 				   db_helper.execute_named_query(NAMED_QUERIES["get_variable_names"], (form_version)).fetchall()])
-	response.append(",".join(header))
+
 	for user in users_to_return:
 		results = {}
-		for result in db_helper.retrieve({"limit": 10000, "users": [user], "fields": ["variable_name", "response"],
+		timestamp = -1
+		results["user"] = '"' + user + '"'
+		for result in db_helper.retrieve({"limit": 10000, "users": [user], "fields": ["variable_name", "response", "timestamp"],
 										  "where": {"form_version": [form_version]}},
 										 "dk_dtu_compute_questionnaire").fetchall():
-			results[result['variable_name']] = '"' + result['response'].replace('"', '').replace("\n", " ").replace(
-				"\r", " ") + '"'
-		line = ['"' + user + '"']
-		line.extend([results.get(variable_name, "") for variable_name in header if variable_name != 'user'])
-		response.append(",".join(line))
-	return "#" + json.dumps({"status": {"code": 200, "message": "OK", "length": str(len(response))}}) + "\n" + "\n".join(response)
+			results[result['variable_name']] = '"' + result['response'].replace('"', '').replace("\n", " ").replace("\r", " ") + '"'
+			if result['variable_name'] == '_submitted' and result['response'] == "true":
+				timestamp = int(time.mktime(result['timestamp'].timetuple()))
+		results["timestamp"] = '"' + str(timestamp) + '"'
+
+		missing_variable_names = missing_variable_names =  set(all_variable_names) - set(results.keys())
+		for variable_name in missing_variable_names:
+			results[variable_name] = ""
+		response.append(results)
+
+	return {"results": response, "meta": {}}
+
